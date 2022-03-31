c9fac72ae7dc2aab4b6e47df4e834857108ab50a
Shawn <41995051+xephora@users.noreply.github.com>
2022-03-31 08:20:50 -0400

    Create Spring4Shell_scanner_nix.sh

---
+++ Threat-Track/Spring4Shell_scanner_nix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Identifying Spring Framework: "
+find / -name spring-beans-*.jar 2>/dev/null
+find / 2>/dev/null | grep "CachedIntrospectionResuLts.class"
+echo -e "\nIdentifying War Files on System: "
+find / -name *.war 2>/dev/null
+echo -e "\nChecking War Files for spring.."
+x=$(find / -name *.war 2>/dev/null)
+for i in $x; do jar tf $i | find -name spring-beans-*.jar; done
