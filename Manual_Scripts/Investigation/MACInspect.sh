96af0bca0102f8bc4cdb661618dfab7b64f3debc
Shawn <41995051+xephora@users.noreply.github.com>
2022-04-05 14:10:06 -0400

    Create MACInspect.sh

---
+++ Manual_Scripts/Investigation/MACInspect.sh
@@ -0,0 +1,47 @@
+# Investigate the following account below.  Please replace `<username>` with the target username below:
+username="<USERNAME>"
+
+echo "Checking cronjobs"
+echo "/var/at/tabs"
+ls -l /var/at/tabs
+echo -e "\n\n"
+echo "Checking System Startups"
+echo "/Library/LaunchAgents"
+ls -l /Library/LaunchAgents
+echo -e "\n\n"
+echo "Checking System Services"
+echo "/Library/LaunchDaemons"
+ls -l /Library/LaunchDaemons
+echo -e "\n\n"
+echo "Checking user downloads folder for suspicious install packages"
+ls -l /Users/$username/downloads/*pkg
+ls -l /Users/$username/downloads/*.dmg
+echo -e "\n\n"
+echo "Checking user $username application data folder"
+echo "/Users/$username/Library"
+ls -l "/Users/$username/Library"
+echo -e "\n\n"
+echo "/Users/$username/Library/Application Support"
+ls -l "/Users/$username/Library/Application Support"
+echo -e "\n\n"
+echo "Checking user startup location"
+echo "/Users/$username/Library/LaunchAgents"
+ls -l "/Users/$username/Library/LaunchAgents"
+echo -e "\n\n"
+echo "Checking Applications"
+echo "/Applications"
+ls -l /Applications
+echo -e "\n\n"
+echo "Checking User specific Processes"
+ps aux | grep "Application Support"
+echo -e "\n\n"
+echo "Checking active processes associated with an application"
+ps aux | grep "/Applications/"
+echo -e "\n\n"
+echo "Checking connections associated with applications within users profile"
+netstat -tunl | grep "/Library/Application Support\|/tmp/"
+echo -e "\n\n"
+echo "Checking root profile"
+echo "/var/root"
+ls -l /var/root
+echo -e "\n\n"
