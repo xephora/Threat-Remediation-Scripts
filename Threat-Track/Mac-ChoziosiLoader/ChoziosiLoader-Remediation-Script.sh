22dd4beecf440d578f5506ea729e34d5d177c8fb
Shawn <41995051+xephora@users.noreply.github.com>
2022-05-10 17:32:36 -0400

    Create ChoziosiLoader-Remediation-Script.sh

---
+++ Threat-Track/Mac-ChoziosiLoader/ChoziosiLoader-Remediation-Script.sh
@@ -0,0 +1,18 @@
+###
+# TODO:
+#  - Experiencing issues with if statements within CrowdStrike environment so manual remediation script was created in the mean-while.
+#  - This is a CrowdStrike script which CrowdStrike successfully blocks the activity of persistence.  The LaunchAgents are failing to execute which means the malicious extensions are failing to load.
+# NOTES: This script is at early stages and will continue to improve.
+###
+username="USERNAME"
+cat "/users/$username/Library/LaunchAgents/com.chrome.extensionsPop.plist" 2>/dev/null | grep "echo" | awk '{print $2}' | base64 -d | awk '{print $8}' | sed 's/:\/\///g' | sed 's/;//g' 
+rm "/Volumes/Application Installer/ChromeInstaller.command" 2>/dev/null
+umount -f "/Volumes/Application Installer" 2>/dev/null
+shasum -a 256 "/Users/$username/Library/LaunchAgents/com.chrome.extension.plist" 2>/dev/null
+shasum -a 256 "/Users/$username/Library/LaunchAgents/com.chrome.extensions.plist" 2>/dev/null
+shasum -a 256 "/Users/$username/Library/LaunchAgents/com.chrome.extensionsPop.plist" 2>/dev/null
+shasum -a 256 "/Users/$username/Downloads/*.dmg" 2>/dev/null
+rm "/Users/$username/Library/LaunchAgents/com.chrome.extension.plist" 2>/dev/null
+rm "/Users/$username/Library/LaunchAgents/com.chrome.extensions.plist" 2>/dev/null
+rm "/Users/$username/Library/LaunchAgents/com.chrome.extensionsPop.plist" 2>/dev/null
+rm "/Users/$username/downloads/*.dmg" 2>/dev/null
