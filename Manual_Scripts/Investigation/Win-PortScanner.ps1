e32e07a2f20470255d00d0d1052d47d155edd254
Shawn <41995051+xephora@users.noreply.github.com>
2022-08-17 13:57:02 -0400

    Create Win-PortScanner.ps1

---
+++ Manual_Scripts/Investigation/Win-PortScanner.ps1
@@ -0,0 +1,10 @@
+# Port scan a neighboring device using CrowdStrike.
+"Company X is Performing Port Scan (Safe to Ignore)"
+$ip = "ENTERIPHERE"
+$ports = @(53,22,80,443,445,3389,5958)
+foreach ($port in $ports) {
+	$x = test-netconnection -ComputerName $ip -Port $port -InformationLevel "Detailed"
+	if ($x.TcpTestSucceeded) {
+		"$ip`:$port is reachable"
+	}
+}
