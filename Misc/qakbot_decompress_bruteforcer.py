a53c69e0f5e9d1ef5484b17433307b2f5d34d0fb
Shawn <41995051+xephora@users.noreply.github.com>
2022-10-07 17:03:03 -0400

    Update qakbot_decompress_bruteforcer.py

--- Misc/qakbot_decompress_bruteforcer.py
+++ Misc/qakbot_decompress_bruteforcer.py
@@ -1,6 +1,8 @@
+# Script was based on the information from Twitter post: https://twitter.com/ankit_anubhav/status/1578257383133876225
 import sys
 import base64
 from zipfile import ZipFile as zip
+import random
 
 def variant1_decompression(html):
         f = open(f"{html}","rb")
@@ -10,16 +12,16 @@ def variant1_decompression(html):
         password = html_read.split("<p class=\"box\">")[1].split("</p>")[0].encode()
         stage1_decoded = str(base64.b64decode(b64_blob))
         stage2_decoded = base64.b64decode(stage1_decoded.split("content = \\\'")[1].split("\\\';")[0])
+        attachment_name = f"attachment{str(random.randint(1,1000))}.zip"
         if "504b03" in stage2_decoded.hex()[:6]:
-                f = open("attachment.zip","wb")
+                f = open(attachment_name,"wb")
                 f.write(stage2_decoded)
                 f.close()
-                with zip("attachment.zip","r") as f:
+                with zip(attachment_name,"r") as f:
                         f.extractall(pwd=password)
         else:
                 print("[+] stage2 decode integrity check failed. No magic bytes 504b03 detected.")
                 quit()
-
 def variant2_decompression(html):
         f = open(f"{html}","rb")
         html_read = str(f.read())
@@ -28,11 +30,12 @@ def variant2_decompression(html):
         password = html_read.split("<p class=\"box\">")[1].split("</p>")[0].encode()
         stage1_decoded = str(base64.b64decode(b64_blob[::-1]))
         stage2_decoded = base64.b64decode(stage1_decoded.split("content = \\\'")[1].split("\\\';")[0])
+        attachment_name = f"attachment{str(random.randint(1,1000))}.zip"
         if "504b03" in stage2_decoded.hex()[:6]:
-                f = open("attachment.zip","wb")
+                f = open(attachment_name,"wb")
                 f.write(stage2_decoded)
                 f.close()
-                with zip("attachment.zip","r") as f:
+                with zip(attachment_name,"r") as f:
                         f.extractall(pwd=password)
         else:
                 print("[+] stage2 decode integrity check failed. No magic bytes 504b03 detected.")
@@ -46,11 +49,12 @@ def variant3_decompression(html):
         password = html_read.split("<p class=\"box\">")[1].split("</p>")[0].encode()
         stage1_decoded = str(base64.b64decode(b64_blob))
         stage2_decoded = base64.b64decode(stage1_decoded.split("content = \\\'")[1].split("\\\';")[0])
+        attachment_name = f"attachment{str(random.randint(1,1000))}.zip"
         if "504b03" in stage2_decoded.hex()[:6]:
-                f = open("attachment.zip","wb")
+                f = open(attachment_name,"wb")
                 f.write(stage2_decoded)
                 f.close()
-                with zip("attachment.zip","r") as f:
+                with zip(attachment_name,"r") as f:
                         f.extractall(pwd=password)
         else:
                 print("[+] stage2 decode integrity check failed. No magic bytes 504b03 detected.")
@@ -61,12 +65,12 @@ if __name__ == "__main__":
         try:
             variant1_decompression(html)
         except:
-            print("[!] attempt 1 failed")
+            pass
         try:
             variant2_decompression(html)
         except:
-            print("[!] attempt 2 failed")
+            pass
         try:
             variant3_decompression(html)
         except:
-            print("[!] attempt 3 failed")
+            pass
