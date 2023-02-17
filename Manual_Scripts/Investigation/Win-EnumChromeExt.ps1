892eab77abb925cb00e9db975780c0f9ad098e1a
Shawn <41995051+xephora@users.noreply.github.com>
2023-02-16 19:34:35 -0500

    Update Win-EnumChromeExt.ps1

--- Manual_Scripts/Investigation/Win-EnumChromeExt.ps1
+++ Manual_Scripts/Investigation/Win-EnumChromeExt.ps1
@@ -1,28 +1,29 @@
-$users = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
-foreach ($user in $users) {
-	if ($user -notlike "*Public*") {
-        $defaultprofileExists = test-path -path "C:\Users\$user\appdata\local\Google\chrome\User Data\default\extensions"
-        if ($defaultprofileExists) {
-            "`n[+] extensions on default profile $user"
-            $defaultExtensions = @(gci "C:\Users\$user\appdata\local\Google\chrome\User Data\default\extensions" -r -fi "manifest.json" | % { $_.FullName})
-            foreach ($dprofile in $defaultExtensions) {
+# Uncomment extcjson to show manifest details for all extensions for target user
+
+$user = "<username>"
+
+if (test-path "C:\Users\$user\appdata\local\Google\chrome\User Data\default") {
+    $defaultExtensions = @(gci "C:\Users\$user\appdata\local\Google\chrome\User Data\default\extensions" -r -fi "manifest.json" | % { $_.FullName})
+    foreach ($extension in $defaultExtensions) {
+        if (test-path $extension) {
+            foreach ($dprofile in $extension) {
                 "`nPath: $dprofile"
-                $extcjson = cat "$dprofile"
-                $extcjson | ForEach-Object {"$PSItem"}
-            }
-        }
-        $profiles = @(gci "C:\Users\$user\appdata\local\Google\chrome\User Data\Profile*").Name
-        foreach ($profile in $profiles) {
-            $profileExists = test-path -path "C:\Users\$user\appdata\local\Google\chrome\User Data\$profile"
-            if ($profileExists) {
-                "`n[+] chrome profiles on profile $user"
-                $ProfilesExtensions = @(gci "C:\Users\$user\appdata\local\Google\chrome\User Data\$profile\extensions" -r -fi "manifest.json" | % { $_.FullName})
-                foreach ($cprofile in $ProfilesExtensions) {
-                    "`nPath: $cprofile"
-                    $extcjson = cat "$cprofile"
-                    $extcjson | ForEach-Object {"$PSItem"}
-                }
+                #$extcjson = cat "$dprofile"
+                #$extcjson | ForEach-Object {"$PSItem"}
             }
         } 
+    }
+}
+
+$profiles = @(gci "C:\Users\$user\appdata\local\Google\chrome\User Data\Profile*").Name
+foreach ($profile in $profiles) {
+    if(test-path "C:\Users\$user\appdata\local\Google\chrome\User Data\$profile") {
+        "`n[+] chrome profiles on profile $user"
+        $ProfilesExtensions = @(gci "C:\Users\$user\appdata\local\Google\chrome\User Data\$profile\extensions" -r -fi "manifest.json" | % { $_.FullName})
+        foreach ($cprofile in $ProfilesExtensions) {
+            "`nPath: $cprofile"
+            #$extcjson = cat "$cprofile"
+            #$extcjson | ForEach-Object {"$PSItem"}
+        }
     }
 }
