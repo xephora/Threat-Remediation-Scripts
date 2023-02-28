28420b4b4aa942707566a8e23b905e2e82a29a8b
Shawn <41995051+xephora@users.noreply.github.com>
2023-02-28 18:31:46 -0500

    Create OneLaunch-Remediation-Script.ps1

---
+++ OneLaunch/OneLaunch-Remediation-Script.ps1
@@ -0,0 +1,73 @@
+Get-Process onelaunch -ErrorAction SilentlyContinue | Stop-Process -Force
+Get-Process onelaunchtray -ErrorAction SilentlyContinue | Stop-Process -Force
+Get-Process chromium -ErrorAction SilentlyContinue | Stop-Process -Force
+sleep 2
+
+
+$user_list = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
+foreach ($i in $user_list) {
+    $installers = @(gci C:\users\$i -r -fi "OneLaunch*.exe" | % {$_.FullName})
+    foreach ($install in $installers) {
+        if (test-path -Path $install) {
+            rm $install
+            $installers = @(gci C:\users\$i -r -fi "OneLaunch*.exe" | % {$_.FullName})
+            if (test-path -Path $install) {
+                "Failed to remove: $install"
+            }
+        }
+    }
+    if (test-path -Path "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneLaunch.lnk") {
+        rm "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneLaunch.lnk" -ErrorAction SilentlyContinue
+        if (test-path -Path "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneLaunch.lnk") {
+            "Failed to remove OneLaunch -> C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneLaunch.lnk"
+        }
+    }
+    if (test-path -Path "C:\Users\$i\appdata\local\OneLaunch") {
+        rm "C:\Users\$i\appdata\local\OneLaunch" -Force -Recurse -ErrorAction SilentlyContinue
+        if (test-path -Path "C:\Users\$i\appdata\local\OneLaunch") {
+            "Failed to remove OneLaunch -> C:\Users\$i\appdata\local\OneLaunch"
+        }
+    }
+}
+
+rm "C:\windows\system32\tasks\OneLaunchLaunchTask" -ErrorAction SilentlyContinue
+
+$sid_list = Get-Item -Path "Registry::HKU\*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
+foreach ($i in $sid_list) {
+    if ($i -notlike "*_Classes*") {
+        $regkeys = @(gci "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Uninstall\*_is1")
+        foreach ($key in $regkeys) {
+            if (test-path -Path $key) {
+                Remove-Item -Path "Registry::$key" -Recurse
+                if (test-path -Path $key) {
+                    "Failed to remove OneLaunch -> $key"
+                }
+            }
+        }
+        $keypath = "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run"
+        $keyexists = (Get-Item $keypath).Property -contains "OneLaunch"
+        if ($keyexists -eq $True) {
+            Remove-ItemProperty -Path "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneLaunch" -ErrorAction SilentlyContinue
+            $keyexists = (Get-Item $keypath).Property -contains "OneLaunch"
+            if ($keyexists -eq $True) {
+                "Failed to remove OneLaunch => Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run.OneLaunch"
+            }
+        }
+        $startupkeys = (gi "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run").Property
+        foreach ($key in $startupkeys) {
+            if ($key -like "GoogleChromeAutoLaunch*") {
+                Remove-ItemProperty -Path "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run" -Name "$key" -ErrorAction SilentlyContinue
+            }
+        }
+        if (test-path -path "Registry::$i\Software\OneLaunch") {
+            Remove-Item -Path "Registry::$i\Software\OneLaunch" -Recurse -ErrorAction SilentlyContinue
+            if (test-path -path "Registry::$i\Software\OneLaunch") {
+                "Failed to remove OneLaunch -> Registry::$i\Software\OneLaunch"
+            }
+        }
+    }
+}
+
+if (test-path -Path 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\OneLaunchLaunchTask') {
+    Remove-Item -Path 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\OneLaunchLaunchTask' -Recurse -ErrorAction SilentlyContinue
+}
