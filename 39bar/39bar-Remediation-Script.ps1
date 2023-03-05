629b7f978f764b2f3fbc25cd5a9af259ea0266f0
Shawn <41995051+xephora@users.noreply.github.com>
2023-03-04 20:42:11 -0500

    Update 39bar-Remediation-Script.ps1

--- 39bar/39bar-Remediation-Script.ps1
+++ 39bar/39bar-Remediation-Script.ps1
@@ -1,41 +1,22 @@
-# Kill processes
-
-get-process -name 39barsvc -ErrorAction SilentlyContinue | Stop-process -Force
-get-process -name 39medint -ErrorAction SilentlyContinue | Stop-process -Force
+Get-Process 39barsvc -ErrorAction SilentlyContinue | Stop-Process -Force
+Get-Process 39medint -ErrorAction SilentlyContinue | Stop-Process -Force
 sleep 2
 
-# Removal from file system
-
-rm "C:\Program Files (x86)\MapsGalaxy_39" -force -recurse -ErrorAction SilentlyContinue
-
-# Removal from registry
-
-$regkeys = get-item -path "registry::hku\*" -ErrorAction SilentlyContinue | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
-
-foreach ($reg in $regkeys) {
-    if ($reg -notlike "*_Classes") {
-        remove-item -path "Registry::$reg\software\MapsGalaxy_39" -recurse -ErrorAction SilentlyContinue
+if (test-path "C:\Program Files (x86)\MapsGalaxy_39") {
+    rm "C:\Program Files (x86)\MapsGalaxy_39" -force -recurse -ErrorAction SilentlyContinue
+    if (test-path "C:\Program Files (x86)\MapsGalaxy_39") {
+        "Failed to remove 39bar -> C:\Program Files (x86)\MapsGalaxy_39"
     }
 }
 
-# Check removal
-
-$check = Test-Path -path "C:\Program Files (x86)\MapsGalaxy_39" -ErrorAction SilentlyContinue
-if ($check) {
-    "Failed to remove C:\Program Files (x86)\MapsGalaxy_39"
-}
-else {
-    continue
-}
-
-foreach ($reg in $regkeys) {
-    if ($reg -notlike "*_Classes") {
-        $check3 = Test-Path -path "Registry::$reg\Software\MapsGalaxy_39" -ErrorAction SilentlyContinue
-        if ($check3 -eq "True") {
-            "This script failed to remove Registry::$reg\Software\MapsGalaxy_39"
-        }
-        else {
-            continue
+$sid_list = Get-Item -Path "Registry::HKU\*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
+foreach ($i in $sid_list) {
+    if ($i -notlike "*_Classes*") {
+        if (test-path "Registry::$i\Software\MapsGalaxy_39") {
+            Remove-Item -Path "Registry::$i\Software\MapsGalaxy_39" -Recurse -ErrorAction SilentlyContinue
+            if (test-path "Registry::$i\Software\MapsGalaxy_39") {
+                "Failed to remove 39bar -> Registry::$i\Software\MapsGalaxy_39"
+            }
         }
     }
 }
