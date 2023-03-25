d517c75720c28998389f701e4e6e0349abbd9477
Shawn <41995051+xephora@users.noreply.github.com>
2023-03-25 10:55:51 -0400

    Create WinInspect_v4.ps1

---
+++ Manual_Scripts/Investigation/WinInspect_v4.ps1
@@ -0,0 +1,297 @@
+# investigate the following account below.  Please replace username with the target account.
+$username = "USERNAME"
+
+# File System - Startups X
+"[+]  Checking Startup Locations"
+$folders = @(
+    "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup",
+    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+)
+$results = @()
+
+foreach ($folder in $folders) {
+    Get-ChildItem -Path $folder -Filter "*.lnk" -Force -ErrorAction SilentlyContinue | ForEach-Object {
+        $shell = New-Object -ComObject WScript.Shell
+        $shortcut = $shell.CreateShortcut($_.FullName)
+        $targetPath = $shortcut.TargetPath
+
+        $result = @{
+            'ShortcutPath' = $_.FullName
+            'TargetPath'   = $targetPath
+        }
+        $results += $result
+    }
+}
+$results | ConvertTo-Json
+
+# File System
+"`n`n [+]  Checking Root Dir"
+"C:\"
+gci C:\ -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+"`n`n [+]  Checking ProgramData"
+"C:\ProgramData"
+gci "C:\ProgramData" -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+"`n`n [+]  Checking Common Files"
+"C:\program files\Common Files"
+gci "C:\program files\Common Files" -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+"`n`n [+]  Checking 32bit Common Files"
+"C:\program files\Common Files (x86)"
+gci "C:\program files (x86)\Common Files" -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+"`n`n [+]  Checking Defender Antivirus Logs"
+"C:\ProgramData\Microsoft\Windows Defender\Support"
+$result = test-path -Path "C:\ProgramData\Microsoft\Windows Defender\Support"
+if ($result -eq "True") {
+	gci "C:\ProgramData\Microsoft\Windows Defender\Support" -fi "*.log" -force -ErrorAction SilentlyContinue | % { $_.FullName, "Size: $($_.length)"}
+} else {
+    "Failed to discover Defender.."
+}
+
+# Public profile
+"`n`n [+]  Checking Public Profile"
+"C:\Users\public"
+gci "C:\Users\public\" -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+"`n`n [+]  Checking Public Desktop"
+"C:\Users\public\Desktop"
+gci "C:\Users\public\Desktop" -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+# User profile
+"`n`n [+]  downloaded executables, msi packages and zip, rar, 7z compressed folders"
+$folders = @("Downloads", "Desktop", "Documents")
+$extensions = @("*.exe", "*.msi", "*.rar", "*.7z", "*.zip", "*.iso", "*.vhd")
+
+foreach ($folder in $folders) {
+    $folderPath = "C:\users\$username\$folder"
+    $fileFound = $false
+    
+    foreach ($extension in $extensions) {
+        $files = Get-ChildItem -Path $folderPath -Recurse -Force -Filter $extension -ErrorAction SilentlyContinue
+        if ($files) {
+            $fileFound = $true
+            break
+        }
+    }
+    
+    if ($fileFound) {
+        "`n$folderPath`n------------------------------------------------" 
+        foreach ($extension in $extensions) {
+            Get-ChildItem -Path $folderPath -Recurse -Force -Filter $extension -ErrorAction SilentlyContinue | ForEach-Object {
+                $_.FullName
+            }
+        }
+    }
+}
+
+# User profile application folders
+"`n`n [+]  Checking User's local application data"
+"C:\Users\$username\appdata\local"
+gci "C:\Users\$username\appdata\local" -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+"`n`n [+]  Checking User's roaming application data"
+"C:\Users\$username\appdata\roaming"
+gci "C:\Users\$username\appdata\roaming" -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+"`n`n [+]  Local Microsoft directory (Solarmarker Backdoor Drop path)"
+"C:\users\$username\AppData\Local\Microsoft"
+gci "C:\users\$username\AppData\Local\Microsoft" -force -ErrorAction SilentlyContinue | % { $_.FullName}
+
+"`n`n [+]  Roaming Microsoft directory (Solarmarker Backdoor Common Drop path)"
+"C:\users\$username\AppData\Roaming\Microsoft"
+gci "C:\users\$username\AppData\Roaming\Microsoft" -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+$exists = test-path "C:\Users\$username\appdata\local\programs"
+if ($exists) {
+    "`n`n Checking User's local Program directory"
+    "C:\Users\$username\appdata\local\programs"
+    gci "C:\users\$username\AppData\Local\programs" -force -ErrorAction SilentlyContinue | % { $_.FullName}
+}
+
+# FileSystem Scheduled Tasks X
+"`n`n [+]  Checking Scheduled Tasks"
+"C:\windows\system32\tasks\"
+gci "C:\windows\system32\tasks\" -force -ErrorAction SilentlyContinue | % { $_.FullName }
+
+"`n`n Checking Scheduled Tasks content (high level)"
+$taskFolderPath = 'C:\Windows\System32\Tasks'
+
+Get-ChildItem -Path $taskFolderPath -ErrorAction SilentlyContinue | ForEach-Object {
+    try {
+        $taskXml = [xml](Get-Content $_.FullName -ErrorAction SilentlyContinue)
+        $taskInfo = @{
+            'TaskName'  = $_.Name
+            'Author'    = $taskXml.Task.RegistrationInfo.Author
+            'UserId'    = $taskXml.Task.Principals.Principal.UserId
+            'Command'   = $taskXml.Task.Actions.Exec.Command
+            'Arguments' = $taskXml.Task.Actions.Exec.Arguments
+        }
+        if ($taskInfo.Command){
+            $taskInfoJson = $taskInfo | ConvertTo-Json
+            $taskInfoJson
+            "--------------------------------------------------------------------------"
+        }
+    } catch {
+    }
+}
+
+# System Registry Scheduled tasks
+"`n`n [+]  Checking System Registries - Scheduled Tasks"
+"Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE"
+(get-item -Path "Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE").GetSubKeyNames()
+
+# System Registries
+"`n`n [+]  Checking System Registries SOFTWARE"
+Get-Item -Path HKLM:\Software\* | % { $_.Name }
+
+"`n`n [+]  Checking System Registries Startup"
+"HKLM:\Software\Microsoft\Windows\CurrentVersion\Run"
+(get-item -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Run).GetValueNames()
+
+$sid_list = Get-Item -Path "Registry::HKU\*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
+foreach ($j in $sid_list) {
+    if ($j -notlike "*_Classes*") {
+        "`n`n [+]  Checking User Registries SID"
+        "$j\Software"
+        (Get-Item -Path Registry::$j\Software).GetSubKeyNames()
+        "`n`n [+]  Checking User Startup Registries SID"
+        "$j\Software\Microsoft\Windows\CurrentVersion\Run"
+        (Get-Item -Path Registry::$j\Software\Microsoft\Windows\CurrentVersion\Run).GetValueNames()
+    }
+}
+
+"`n`n[+] Chrome History files"
+$historyFilePaths=@(gci "C:\users\$username\AppData\Local\google\Chrome\User Data" -r -fi "history" | % { $_.FullName }); 
+foreach ($filePath in $historyFilePaths) {
+	if (test-path $filepath) {
+      $fileInfo = Get-Item $filePath | Select-Object FullName, Length, CreationTime, LastWriteTime
+      $fileSizeMB = $fileInfo.Length / 1MB
+      Write-Output "File path: $($fileInfo.FullName)"
+      Write-Output "File size: $fileSizeMB MB"
+      Write-Output "Creation date: $($fileInfo.CreationTime)"
+      Write-Output "Modification date: $($fileInfo.LastWriteTime)"
+      Write-Output "------------------------"
+    }
+}
+
+# Edge and IE History File
+"`n`n[+]  Checking for Internet Explorer / Edge History File"
+$IEHistoryfile = "C:\Users\$username\AppData\Local\Microsoft\Windows\WebCache\WebCacheV01.dat"
+if (test-path $IEHistoryfile) {
+	"Internet Explorer/Microsoft Edge History file in C:\Users\$username\AppData\Local\Microsoft\Windows\WebCache\WebCacheV01.dat"
+	$fileInfo = Get-Item $IEHistoryfile | Select-Object FullName, Length, CreationTime, LastWriteTime
+	$fileSizeMB = $IEHistoryfile.Length / 1MB
+	Write-Output "File path: $($fileInfo.FullName)"
+	Write-Output "File size: $fileSizeMB MB"
+	Write-Output "Creation date: $($fileInfo.CreationTime)"
+	Write-Output "Modification date: $($fileInfo.LastWriteTime)"
+	Write-Output "------------------------"
+}
+
+# Check for profile processes X
+"`n`n [+] Checking Profile-based processes"
+Get-Process |
+    Select-Object -Property Name, Id, Path |
+    Where-Object {
+        ($_.Path -ilike "*\AppData\Local*") -or
+        ($_.Path -ilike "*\AppData\Roaming*") -or
+        ($_.Path -ilike "ProgramData*")
+}
+
+# Detect Browser Activity X
+"`n`n [+]  Browsers Detected"
+$browserNames = @("chrome", "iexplore", "msedge", "firefox")
+
+foreach ($browser in $browserNames) {
+    Get-Process $browser -ErrorAction SilentlyContinue | ForEach-Object {
+        $_.ProcessName,
+        "PID: $($_.Id)",
+        $_.Description,
+        $_.Company,
+        $_.Path
+    }
+}
+
+# Running Services X
+"`n`n[+] Checking for running services"
+Get-Service | Where-Object { $_.Status -eq "Running" } | ForEach-Object {
+    $serviceInfo = Get-WmiObject -Class Win32_Service -Filter "Name='$($_.Name)'"
+    @{
+        'ServiceName'   = $_.Name
+        'DisplayName'   = $_.DisplayName
+        'Status'        = $_.Status
+        'CanStop'       = $_.CanStop
+        'ServiceBinary' = $serviceInfo.PathName
+    }
+} | ConvertTo-Json
+
+# Enumerate Disk  X
+"`n`n[+] Checking Disks"
+$logicalDisks = Get-WmiObject -Class Win32_LogicalDisk
+$cdromDrives = Get-WmiObject -Class Win32_CDROMDrive
+
+foreach ($disk in $logicalDisks) {
+    $diskInfo = @{
+        'DeviceID'    = $disk.DeviceID
+        'VolumeName'  = $disk.VolumeName
+        'Description' = $disk.Description
+    }
+    
+    $cdromDrive = $cdromDrives | Where-Object { $_.Drive -eq $disk.DeviceID }
+    if ($cdromDrive) {
+        $diskInfo['MountedISO'] = $cdromDrive.MediaLoaded -and $cdromDrive.VolumeName
+    }
+
+    $diskInfo | ConvertTo-Json
+}
+
+"`n`n [+] Enumerating Chrome Extensions"
+if (test-path "C:\Users\$username\appdata\local\Google\chrome\User Data\default") {
+    $defaultExtensions = @(gci "C:\Users\$username\appdata\local\Google\chrome\User Data\default\extensions" -r -fi "manifest.json" | % { $_.FullName})
+    foreach ($extension in $defaultExtensions) {
+        if (test-path $extension) {
+            foreach ($dprofile in $extension) {
+                $extid =  (($dprofile -split '\\extensions')[1] -split '\\')[1]
+                $url = "https://chrome.google.com/webstore/detail/docs/"
+                $eurl = "$($url)$($extid)"
+                try {
+                    $res = Invoke-WebRequest $eurl -ErrorAction Ignore
+                    $content = $res.Content
+                    $srange = $content.IndexOf("<title>")+"<title>".Length
+                    $erange = $content.IndexOf("</title>")
+                    $title = $content.Substring($srange,$erange - $srange)
+                    "`nPath: $dprofile -> $title"
+                } catch {
+                    "`nPath: $dprofile ! Failed to detect!"
+                }
+            }
+        }
+    }
+}
+
+if (test-path "C:\Users\$username\appdata\local\Google\chrome\User Data\Profile*") {
+    $profiles = @(gci "C:\Users\$username\appdata\local\Google\chrome\User Data\Profile*").Name
+    foreach ($profile in $profiles) {
+        if(test-path "C:\Users\$username\appdata\local\Google\chrome\User Data\$profile") {
+            "`n[+] chrome profile $profile on profile $username"
+            $ProfilesExtensions = @(gci "C:\Users\$username\appdata\local\Google\chrome\User Data\$profile\extensions" -r -fi "manifest.json" | % { $_.FullName})
+            foreach ($cprofile in $ProfilesExtensions) {
+                $extid =  (($cprofile -split '\\extensions')[1] -split '\\')[1]
+                $url = "https://chrome.google.com/webstore/detail/docs/"
+                $eurl = "$($url)$($extid)"
+                try {
+                    $res = Invoke-WebRequest $eurl -ErrorAction Ignore
+                    $content = $res.Content
+                    $srange = $content.IndexOf("<title>")+"<title>".Length
+                    $erange = $content.IndexOf("</title>")
+                    $title = $content.Substring($srange,$erange - $srange)
+                    "`nPath: $cprofile -> $title"
+                } catch {
+                    "`nPath: $cprofile ! Failed to detect!"
+                }
+            }
+        }
+    }
+}
