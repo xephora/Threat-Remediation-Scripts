536a82cf35d25b361f269927364dee1ae3500513
Shawn <41995051+xephora@users.noreply.github.com>
2023-03-25 20:12:03 -0400

    Update LinInspect.sh

--- Manual_Scripts/Investigation/LinInspect.sh
+++ Manual_Scripts/Investigation/LinInspect.sh
@@ -1,37 +1,98 @@
-# Early development phase 05-12-2022
-# references https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-RED='\033[0;31m'
-NC='\033[0m'
-echo -e "${RED}[+] Enumerating opt: ${NC}\n"
-ls -l /opt 2>/dev/null
-echo -e "\n${RED}[+] Enumerating shm: {NC}\n"
-ls -l /dev/shm 2>/dev/null
-echo -e "\n${RED}[+] Enumerating root: ${NC}\n"
-echo -e "[+] Root Profile: \n"
-ls -l /root 2>/dev/null
-echo -e "[+] Root Downloads: \n"
-ls -l /root/Downloads 2>/dev/null
-echo -e "[+] Root Documents: \n"
-ls -l /root/Documents 2>/dev/null
-echo -e "[+] Root Desktop: \n"
-ls -l /root/Desktop 2>/dev/null
-echo -e "[+] Root ssh: \n"
-ls -l /root/.ssh 2>/dev/null
-echo -e "\n${RED}[+] Looking for persistence in root bashrc: ${NC}\n"
-cat /root/.bashrc | grep "alias\|export"
-echo -e "\n${RED}[+] Checking user history: ${NC}\n"
-x=$(ls /home); for i in $x; do echo "- [+] Checking profile $i: " && cat /home/$i/.bash_history 2>/dev/null | grep "curl\|$
-echo -e "\n${RED}[+] Checking interesting processes: ${NC}\n"
-x=$(ls /home); for i in $x; do ps -ef --forest | grep $i | grep -v  "kworker\|xfs\|jfs\|grep\|journald\|systemd\|sd-pam\|a$
-echo -e "\n${RED}[+] Checking users for bash scripts, elf binaries, python scripts: ${NC}\n"
-x=$(ls /home); for i in $x; do find /home/$i | grep "*\.sh\|*\.elf\|*\.py"; done
-echo -e "\n${RED}[+] Checking users download folder: ${NC}\n"
-x=$(ls /home); for i in $x; do echo "- [+] Profile $i" && ls /home/$i/downloads 2>/dev/null; done
-echo -e "\n${RED}[+] Enumerating processes associated with wwwroot or mysql: ${NC}\n"
-ps -ef --forest | grep "wwwroot\|mysql"
-echo -e "\n${RED}[+] Enumerating ports: ${NC}\n"
-netstat -tunlp 2>/dev/null
-echo -e "\n${RED}[+] Enumerating IP information ${NC}\n"
-ifconfig 2>/dev/null
+# Script still undergoing work.  It can be used if you please, but still too early.
+echo "[+] Startup paths"
+
+echo "/etc/systemd/system/ -> System-wide custom service files"
+ls -la /etc/systemd/system/ 2>/dev/null
+
+echo "/lib/systemd/system/ -> Default service files provided by packages"
+ls -la /lib/systemd/system/ 2>/dev/null
+
+echo "/etc/init.d/ -> System-wide SysV init script"
+ls -la /etc/init.d/ 2>/dev/null
+
+echo "/etc/xdg/autostart/ -> System-wide autostart files"
+ls -la /etc/xdg/autostart/ 2>/dev/null
+
+echo "/etc/profile -> System-wide login script"
+cat /etc/profile 2>/dev/null
+
+echo "/etc/profile.d/ -> Additional system-wide login scripts"
+
+echo "/etc/bash.bashrc ->  System-wide non-login script"
+
+cat "/etc/bash.bashrc" 2>/dev/null
+
+echo "[+] temp profiles"
+ls -la /tmp/ 2>/dev/null
+ls -la /var/tmp/ 2>/dev/null
+
+echo "[+] persistence"
+
+echo "/etc/cron.d/"
+ls -la /etc/cron.d/ 2>/dev/null
+
+echo "/etc/cron.daily/"
+ls -la /etc/cron.daily/ 2>/dev/null
+
+echo "/etc/cron.hourly/"
+ls -la /etc/cron.hourly/ 2>/dev/null
+
+echo "/etc/cron.weekly/"
+ls -la /etc/cron.weekly/ 2>/dev/null
+
+echo "/etc/cron.monthly/"
+ls -la /etc/cron.monthly/ 2>/dev/null
+
+echo "/var/spool/cron/crontabs/"
+ls -la /var/spool/cron/crontabs/ 2>/dev/null
+
+echo "[+] Removable media
+
+echo "/media/ -> removable media"
+ls -la /media/ 2>/dev/null
+
+user_dirs=$(ls -1 /home/ | grep -v '^root$')
+for username in $user_dirs; do
+    echo "Processing user: $username"
+
+    echo "/home/$username/.config/autostart/ -> user autostart files"
+    ls -la "/home/$username/.config/autostart/" 2>/dev/null
+
+    echo "/home/$username -> user login scripts .bash_profile, .bash_login, and .profile"
+    cat "/home/$username/.bash_profile" 2>/dev/null
+    cat "/home/$username/.bash_login" 2>/dev/null
+    cat "/home/$username/.profile" 2>/dev/null
+    cat "/home/$username/.bashrc" 2>/dev/null
+
+    echo "User media:"
+    ls -la "/media/$username/" 2>/dev/null
+
+    echo "[+] Profile data"
+    ls -la "/home/$username/Downloads" 2>/dev/null
+    ls -la "/home/$username/Documents" 2>/dev/null
+    ls -la "/home/$username/Desktop" 2>/dev/null
+    echo "---------------------------------------------"
+done
+
+echo "[+] Checking mounts"
+mount
+
+echo "[+] Checking disks"
+df -h
+
+echo "[+] Checking for active smb shares"
+cat /etc/samba/smb.conf 2>/dev/null
+
+echo "[+] Checking active NFS shares on your Linux system"
+cat /etc/exports 2>/dev/null
+
+echo "[+] Checking active processes with associated child processes"
+ps -ef --forest
+
+echo "[+] Checking for active services"
+systemctl list-units --type=service --state=active
+
+echo "[+] Checking for users on system"
+cat /etc/passwd
 echo -e "\n${RED}[+] Enumerating passwd file: ${NC}\n"
 cat /etc/passwd 2>/dev/null | grep "/bin/bash\|/bin/sh"
