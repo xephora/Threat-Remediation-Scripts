a8abc19bd522ebc2bf355161f44fa939f1ee77f5
Shawn <41995051+xephora@users.noreply.github.com>
2023-05-25 07:50:58 -0400

    Update OneLaunch-Remediation-Script.ps1

--- OneLaunch/OneLaunch-Remediation-Script.ps1
+++ OneLaunch/OneLaunch-Remediation-Script.ps1
@@ -3,7 +3,6 @@ Get-Process onelaunchtray -ErrorAction SilentlyContinue | Stop-Process -Force
 Get-Process chromium -ErrorAction SilentlyContinue | Stop-Process -Force
 sleep 2
 
-
 $user_list = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
 foreach ($i in $user_list) {
     $installers = @(gci C:\users\$i -r -fi "OneLaunch*.exe" | % {$_.FullName})
@@ -22,6 +21,12 @@ foreach ($i in $user_list) {
             "Failed to remove OneLaunch -> C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneLaunch.lnk"
         }
     }
+    if (test-path -Path "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneLaunchChromium.lnk") {
+        rm "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneLaunchChromium.lnk" -ErrorAction SilentlyContinue
+        if (test-path -Path "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneLaunchChromium.lnk") {
+            "Failed to remove OneLaunch -> C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\OneLaunchChromium.lnk"
+        }
+    }
     if (test-path -Path "C:\Users\$i\appdata\local\OneLaunch") {
         rm "C:\Users\$i\appdata\local\OneLaunch" -Force -Recurse -ErrorAction SilentlyContinue
         if (test-path -Path "C:\Users\$i\appdata\local\OneLaunch") {
@@ -31,6 +36,8 @@ foreach ($i in $user_list) {
 }
 
 rm "C:\windows\system32\tasks\OneLaunchLaunchTask" -ErrorAction SilentlyContinue
+rm "C:\windows\system32\tasks\ChromiumLaunchTask" -ErrorAction SilentlyContinue
+rm "C:\windows\system32\tasks\OneLaunchUpdateTask" -ErrorAction SilentlyContinue
 
 $sid_list = Get-Item -Path "Registry::HKU\*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
 foreach ($i in $sid_list) {
@@ -53,6 +60,15 @@ foreach ($i in $sid_list) {
                 "Failed to remove OneLaunch => Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run.OneLaunch"
             }
         }
+        $keypath = "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run"
+        $keyexists = (Get-Item $keypath).Property -contains "OneLaunchChromium"
+        if ($keyexists -eq $True) {
+            Remove-ItemProperty -Path "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneLaunchChromium" -ErrorAction SilentlyContinue
+            $keyexists = (Get-Item $keypath).Property -contains "OneLaunchChromium"
+            if ($keyexists -eq $True) {
+                "Failed to remove OneLaunch => Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run.OneLaunchChromium"
+            }
+        }
         $startupkeys = (gi "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run").Property
         foreach ($key in $startupkeys) {
             if ($key -like "GoogleChromeAutoLaunch*") {
@@ -70,4 +86,12 @@ foreach ($i in $sid_list) {
 
 if (test-path -Path 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\OneLaunchLaunchTask') {
     Remove-Item -Path 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\OneLaunchLaunchTask' -Recurse -ErrorAction SilentlyContinue
+}
+
+if (test-path -Path 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\ChromiumLaunchTask') {
+    Remove-Item -Path 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\ChromiumLaunchTask' -Recurse -ErrorAction SilentlyContinue
+}
+
+if (test-path -Path 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\OneLaunchUpdateTask') {
+    Remove-Item -Path 'Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\OneLaunchUpdateTask' -Recurse -ErrorAction SilentlyContinue
 }
