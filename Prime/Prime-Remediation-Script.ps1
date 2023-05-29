3fd9f49079645ab141c924c1080eed2775f3791f
Shawn <41995051+xephora@users.noreply.github.com>
2023-05-29 07:23:56 -0400

    Update Prime-Remediation-Script.ps1

--- Prime/Prime-Remediation-Script.ps1
+++ Prime/Prime-Remediation-Script.ps1
@@ -2,23 +2,30 @@ Get-Process Prime -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorActi
 sleep 2
 
 $user_list = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
-
 foreach ($i in $user_list) {
     if ($i -notlike "*Public*") {
         $exists = test-path "C:\users\$i\appdata\local\Prime"
-        $exists2 = test-path "C:\users\$i\appdata\roaming\Prime"
         if ($exists -eq $True) {
             rm "C:\users\$i\appdata\local\Prime" -Force -Recurse -ErrorAction SilentlyContinue 
-            $exists = test-path "C:\users\$i\appdata\local\Prime"
+            $exist = test-path "C:\users\$i\appdata\local\Prime"
             if ($exists -eq $True) {
                 "Prime Removal Unsuccessful => C:\users\$i\appdata\local\Prime" 
             }
         } 
-        if ($exists2 -eq $True) {
+        $exists = test-path "C:\users\$i\appdata\roaming\Prime"
+        if ($exists -eq $True) {
             rm "C:\users\$i\appdata\roaming\Prime" -Force -Recurse -ErrorAction SilentlyContinue 
-            $exists2 = test-path "C:\users\$i\appdata\roaming\Prime"
-            if ($exists2 -eq $True) {
+            $exists = test-path "C:\users\$i\appdata\roaming\Prime"
+            if ($exists -eq $True) {
                 "Prime Removal Unsuccessful => C:\users\$i\appdata\roaming\Prime"
+            }
+        } 
+        $exists = test-path "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\prime.lnk"
+        if ($exists -eq $True) {
+            rm "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\prime.lnk" -ErrorAction SilentlyContinue 
+            $exists = test-path "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\prime.lnk"
+            if ($exists -eq $True) {
+                "Prime Removal Unsuccessful => C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\prime.lnk"
             }
         } 
     }
@@ -28,12 +35,12 @@ $sid_list = Get-Item -Path "Registry::HKU\*" | Select-String -Pattern "S-\d-(?:\
 foreach($j in $sid_list) {
     if ($j -notlike "*_Classes*") {
     	$regkey = "Registry::$j\Software\Microsoft\Windows\CurrentVersion\Run"
-    	$exists3 = (Get-Item $regkey).Property -contains "Prime"
-        if ($exists3 -eq $True) {
+    	$exists = (Get-Item $regkey).Property -contains "Prime"
+        if ($exists -eq $True) {
             Remove-ItemProperty -Path "Registry::$j\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Prime" -ErrorAction SilentlyContinue
             $regkey = "Registry::$j\Software\Microsoft\Windows\CurrentVersion\Run"
-    	    $exists3 = (Get-Item $regkey).Property -contains "Prime"
-            if ($exists3 -eq $True) {
+    	    $exists = (Get-Item $regkey).Property -contains "Prime"
+            if ($exists -eq $True) {
                 "Prime Removal Unsuccessful => Registry::$j\Software\Microsoft\Windows\CurrentVersion\Run.Prime"
             }
         }
