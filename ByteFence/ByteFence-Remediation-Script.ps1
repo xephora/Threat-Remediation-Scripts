7aac3b80ff05b72dbc712fcb940f0e088d9bef38
Shawn <41995051+xephora@users.noreply.github.com>
2023-06-27 21:25:29 -0400

    Update ByteFence-Remediation-Script.ps1

--- ByteFence/ByteFence-Remediation-Script.ps1
+++ ByteFence/ByteFence-Remediation-Script.ps1
@@ -1,66 +1,30 @@
-# Terminate Processes
+$service = Get-Service -Name 'ByteFenceService' -ErrorAction SilentlyContinue
+if ($service) { $service | Stop-Service -Force -ErrorAction SilentlyContinue }
 
-Get-Process ByteFenceService -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-Get-Process rtop_bg -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-Get-Process rtop_svc -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-
-# Stopping and Removing Services
-
-Stop-Service -Name ByteFenceService -Force -ErrorAction SilentlyContinue
-#Remove-Service -Name ByteFenceService -Force -ErrorAction SilentlyContinue
-# Remove-Service cmdlet was introduced in PowerShell 6.0 and may not work on certain devices.  If the device has Powershell 6.0 or greater, please uncomment the above line.
-# https://github.com/MicrosoftDocs/PowerShell-Docs/issues/4510
-sleep 2
-
-# Removal from file system
-
-rm "C:\Program Files\ByteFence" -Force -Recurse -ErrorAction SilentlyContinue
-rm "C:\ProgramData\ByteFence" -Force -Recurse -ErrorAction SilentlyContinue
-rm "C:\windows\system32\tasks\ByteFence" -ErrorAction SilentlyContinue
-
-# Removal from registries
-
-Remove-Item -path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\ByteFence" -Recurse -ErrorAction SilentlyContinue
-Remove-Item -path "Registry::HKEY_LOCAL_MACHINE\Software\ByteFence" -Recurse -ErrorAction SilentlyContinue
-
-# Check removal
-
-$check1 = Test-Path -path "C:\Program Files\ByteFence" -ErrorAction SilentlyContinue
-if ($check1) {
-    "Script failed to remove C:\Program Files\ByteFence"
-}
-else {
-    continue
+if ($PSVersionTable.PSVersion.Major -eq 6 -and $PSVersionTable.PSVersion.Minor -eq 0) {
+    Remove-Service -Name ByteFenceService -Force -ErrorAction SilentlyContinue
 }
 
-$check2 = Test-Path -path "C:\ProgramData\ByteFence" -ErrorAction SilentlyContinue
-if ($check2) {
-    "Script failed to remove C:\ProgramData\ByteFence"
-}
-else {
-    continue
+$processes = @( 'ByteFenceService', 'rtop_bg', 'rtop_svc' )
+foreach ($process in $processes) {
+    $proc = Get-Process $process -ErrorAction SilentlyContinue
+    if ($proc) { $proc | Stop-Process -Force -ErrorAction SilentlyContinue }
 }
 
-$check3 = Test-Path -path "C:\windows\system32\tasks\ByteFence" -ErrorAction SilentlyContinue
-if ($check3) {
-    "Script failed to remove C:\windows\system32\tasks\ByteFence"
-}
-else {
-    continue
-}
+Start-Sleep -Seconds 2
+$paths = @( 
+    "C:\Program Files\ByteFence",
+    "C:\ProgramData\ByteFence",
+    "C:\windows\system32\tasks\ByteFence",
+    "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\ByteFence",
+    "Registry::HKEY_LOCAL_MACHINE\Software\ByteFence"
+)
 
-$check4 = Test-Path -path "Registry::HKEY_LOCAL_MACHINE\Software\ByteFence" -ErrorAction SilentlyContinue
-if ($check4) {
-    "Script failed to remove Registry::HKEY_LOCAL_MACHINE\Software\ByteFence"
-}
-else {
-    continue
-}
-
-$check5 = Test-Path -path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\ByteFence" -ErrorAction SilentlyContinue
-if ($check5) {
-    "Script failed to remove Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\ByteFence"
-}
-else {
-    continue
+foreach ($path in $paths) {
+    if (Test-Path $path) {
+        Remove-Item $path -Force -Recurse -ErrorAction SilentlyContinue
+        if (Test-Path $path) {
+            "Script failed to remove $path"
+        }
+    }
 }
