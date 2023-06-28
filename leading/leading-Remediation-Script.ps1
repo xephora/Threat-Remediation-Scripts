a1577027524800f55c0c73e3db9fe45899956da8
Shawn <41995051+xephora@users.noreply.github.com>
2023-06-27 21:13:47 -0400

    Update leading-Remediation-Script.ps1

--- leading/leading-Remediation-Script.ps1
+++ leading/leading-Remediation-Script.ps1
@@ -1,49 +1,31 @@
-Get-Process Leading -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-sleep 2
+$LeadingProcess = Get-Process Leading -ErrorAction SilentlyContinue
+if ($LeadingProcess) { $LeadingProcess | Stop-Process -Force -ErrorAction SilentlyContinue; Start-Sleep -Seconds 2 }
 
-$user_list = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
+$user_list = Get-ChildItem 'C:\users\*' | Where-Object { $_.Name -ne 'Public' } | ForEach-Object { $_.Name }
+foreach ($user in $user_list) {
+    $paths = @( 
+        "C:\users\$user\appdata\local\Leading", 
+        "C:\users\$user\appdata\roaming\Leading",
+        "C:\Users\$user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Leading.lnk"
+    )
 
-foreach ($i in $user_list) {
-    if ($i -notlike "*Public*") {
-        $exists = test-path "C:\users\$i\appdata\local\Leading"
-        $exists2 = test-path "C:\users\$i\appdata\roaming\Leading"
-        $exists4 = test-path "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Leading.lnk"
-        if ($exists -eq $True) {
-            rm "C:\users\$i\appdata\local\Leading" -Force -Recurse -ErrorAction SilentlyContinue 
-            $exists = test-path "C:\users\$i\appdata\local\Leading"
-            if ($exists -eq $True) {
-                "Leading Removal Unsuccessful => C:\users\$i\appdata\local\Leading" 
+    foreach ($path in $paths) {
+        if (Test-Path $path) {
+            Remove-Item $path -Force -Recurse -ErrorAction SilentlyContinue
+            if (Test-Path $path) {
+                "Leading Removal Unsuccessful => $path"
             }
-        } 
-        if ($exists2 -eq $True) {
-            rm "C:\users\$i\appdata\roaming\Leading" -Force -Recurse -ErrorAction SilentlyContinue 
-            $exists2 = test-path "C:\users\$i\appdata\roaming\Leading"
-            if ($exists2 -eq $True) {
-                "Leading Removal Unsuccessful => C:\users\$i\appdata\roaming\Leading"
-            }
-        }
-            if ($exists4 -eq $True) {
-            rm "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Leading.lnk" -Force -Recurse -ErrorAction SilentlyContinue 
-            $exists4 = test-path "C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Leading.lnk"
-            if ($exists4 -eq $True) {
-               "Leading Removal Unsuccessful => C:\Users\$i\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Leading.lnk"
-             }
         }
     }
 }
 
-$sid_list = Get-Item -Path "Registry::HKU\S-*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
-foreach($j in $sid_list) {
-    if ($j -notlike "*_Classes*") {
-    	$regkey = "Registry::$j\Software\Microsoft\Windows\CurrentVersion\Run"
-    	$exists3 = (Get-Item $regkey).Property -contains "Leading"
-        if ($exists3 -eq $True) {
-            Remove-ItemProperty -Path "Registry::$j\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Leading" -ErrorAction SilentlyContinue
-            $regkey = "Registry::$j\Software\Microsoft\Windows\CurrentVersion\Run"
-    	    $exists3 = (Get-Item $regkey).Property -contains "Leading"
-            if ($exists3 -eq $True) {
-                "Leading Removal Unsuccessful => Registry::$j\Software\Microsoft\Windows\CurrentVersion\Run.Leading"
-            }
+$sid_list = Get-ChildItem -Path "Registry::HKU\S-*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+" | Where-Object { $_ -notlike "*_Classes*" }
+foreach($sid in $sid_list) {
+    $regkey = "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Run"
+    if ((Get-Item $regkey).Property -contains "Leading") {
+        Remove-ItemProperty -Path $regkey -Name "Leading" -ErrorAction SilentlyContinue
+        if ((Get-Item $regkey).Property -contains "Leading") {
+            "Leading Removal Unsuccessful => $regkey.Leading"
         }
     }
 }
