04fed92eddd61f02d4caf69ac0676aabe5837ae0
Shawn <41995051+xephora@users.noreply.github.com>
2023-07-26 07:40:45 -0400

    Update Win-PortScanner.ps1

--- Manual_Scripts/Investigation/Win-PortScanner.ps1
+++ Manual_Scripts/Investigation/Win-PortScanner.ps1
@@ -1,7 +1,7 @@
 # Port scan a neighboring device using CrowdStrike.
 "Company X is Performing Port Scan (Safe to Ignore)"
 $ip = "ENTERIPHERE"
-$ports = @(53,22,80,443,445,3389,5958)
+$ports = @(53,22,80,443,445,3389,5985)
 foreach ($port in $ports) {
 	$x = test-netconnection -ComputerName $ip -Port $port -InformationLevel "Detailed"
 	if ($x.TcpTestSucceeded) {
