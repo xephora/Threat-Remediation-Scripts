b6b46b831a38225ce5711cbb4af4f029ff2c51bd
Shawn <41995051+xephora@users.noreply.github.com>
2023-12-02 09:27:27 -0500

    Update PCAppStore-Remediation-Script.ps1

--- PCAppStore/PCAppStore-Remediation-Script.ps1
+++ PCAppStore/PCAppStore-Remediation-Script.ps1
@@ -1,53 +1,65 @@
-Get-Process PCAppStore -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-Get-Process NW_store -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+$process = Get-Process PCAppStore -ErrorAction SilentlyContinue
+if ($process) { $process | Stop-Process -Force -ErrorAction SilentlyContinue; Start-Sleep -Seconds 2 }
+$process = Get-Process NW_store -ErrorAction SilentlyContinue
+if ($process) { $process | Stop-Process -Force -ErrorAction SilentlyContinue; Start-Sleep -Seconds 2 }
 sleep 2
 
 $user_list = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
-foreach ($i in $user_list) {
-    if ($i -notlike "*Public*") {
-        $exists = test-path "C:\Users\$i\PCAppStore"
-        $exists2 = test-path "C:\Users\$i\AppData\Roaming\PCAppStore"
-        if ($exists -eq $True) {
-            rm "C:\Users\$i\PCAppStore" -Force -Recurse -ErrorAction SilentlyContinue 
-            $exists = test-path "C:\Users\$i\PCAppStore"
-            if ($exists -eq $True) {
-                "PCAppStore Removal Unsuccessful => C:\Users\$i\PCAppStore" 
+foreach ($user in $user_list) {
+    if ($user -notlike "*Public*") {
+        $paths = @(
+            "C:\Users\$user\PCAppStore",
+            "C:\Users\$user\AppData\roaming\PCAppStore",
+            "C:\Users\$user\Appdata\local\pc_app_store"
+        )
+        foreach ($path in $paths) {
+            if (test-path $path) {
+                rm -Path $path -Recurse -ErrorAction SilentlyContinue
+                if (test-path $path) {
+                    "[!] Failed to remove PCAppstore -> $path"
+                }
             }
         }
-        if ($exists2 -eq $True) {
-            rm "C:\Users\$i\AppData\Roaming\PCAppStore" -Force -Recurse -ErrorAction SilentlyContinue 
-            $exists2 = test-path "C:\Users\$i\AppData\Roaming\PCAppStore"
-            if ($exists2 -eq $True) {
-                "PCAppStore Removal Unsuccessful => C:\Users\$i\AppData\Roaming\PCAppStore"
+        $path = "C:\Users\$user\downloads\Zoom-Setup-PCAppStore*.exe"
+        if (test-path $path) {
+            rm $path -ErrorAction SilentlyContinue
+            if (test-path $path) {
+                $file = ls $path -ErrorAction SilentlyContinue
+                "[!] Failed to remove PCAppStore installer => $file"
             }
-        }
-        $exists3 = test-path -path "C:\Users\$i\AppData\Local\Temp\z.exe"
-        if ($exists3 -eq $True) {
-            rm "C:\Users\$i\AppData\Local\Temp\z.exe"
-            # Adware PCAppStore attempts to add an exclusion to Windows Defender for 'z.exe' in the 'temp' directory of the affected user profile.  If the file in question exists, then remove the file and the exclusion.
-            Remove-MpPreference -ExclusionProcess "C:\Users\$i\AppData\Local\Temp\z.exe" -ErrorAction SilentlyContinue
-            $exists3 = test-path -path "C:\Users\$i\AppData\Local\Temp\z.exe"
-            if ($exists3 -eq $True) {
-                "PCAppStore Removal Unsuccessful => C:\Users\$i\AppData\Local\Temp\z.exe"
-            }
-        }
-        $exists4 = test-path -Path "C:\Users\$i\downloads\Zoom-Setup-PCAppStore*.exe"
-        if ($exists4 -eq $True) {
-            rm "C:\Users\$i\Zoom-Setup-PCAppStore*.exe" -ErrorAction SilentlyContinue
         }
     }
 }
 
-$sid_list = Get-Item -Path "Registry::HKU\*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
-foreach($j in $sid_list) {
-    if ($j -notlike "*_Classes*") {
-        $exists = test-path -path "Registry::$j\SOFTWARE\PCAppStore"
-        if ($exists -eq $True) {
-            Remove-Item -Path "Registry::$j\SOFTWARE\PCAppStore" -Recurse -ErrorAction SilentlyContinue
-            $exists = test-path -path "Registry::$j\SOFTWARE\PCAppStore"
-            if ($exists -eq $True) {
-                "PCAppStore Removal Unsuccessful => Registry::$j\SOFTWARE\PCAppStore"
+$sid_list = Get-Item -Path "Registry::HKU\-*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+"
+foreach ($sid in $sid_list) {
+    if ($sid -notlike "*_Classes*") {
+        $paths = @(
+            "Registry::$sid\SOFTWARE\PCAppStore",
+            "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Run\PCApp"
+        )
+        foreach ($path in $paths) {
+            write-host "Cleaning $path"
+            if (test-path $path) {
+                Remove-Item -Path $path -Recurse -ErrorAction SilentlyContinue
+                if (test-path $path) {
+                    "[!] Failed to remove PCAppstore => $path"
+                }
             }
         }
+    }
+}
+
+if (test-path "C:\windows\system32\tasks\PCAppStoreAutoUpdater") {
+    rm "C:\windows\system32\tasks\PCAppStoreAutoUpdater" -ErrorAction SilentlyContinue
+    if (test-path "C:\windows\system32\tasks\PCAppStoreAutoUpdater") {
+        "[!] Failed to remove PCAppstore => C:\windows\system32\tasks\PCAppStoreAutoUpdater"
+    }
+}
+
+if (test-path "Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\PCAppStoreAutoUpdater") {
+    rm "Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\PCAppStoreAutoUpdater" -Recurse -ErrorAction SilentlyContinue
+    if (test-path "Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\PCAppStoreAutoUpdater") {
+        "[!] Failed to remove PCAppstore => Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\PCAppStoreAutoUpdater"
     }
 }
