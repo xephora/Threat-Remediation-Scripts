2aa52bfbd2628095f156897e2bc1dceaeba9b6aa
Shawn <41995051+xephora@users.noreply.github.com>
2024-04-28 10:51:57 -0400

    Update asyncrat_stage2_decryption.py

--- Threat-Track/ASyncRAT/asyncrat_stage2_decryption.py
+++ Threat-Track/ASyncRAT/asyncrat_stage2_decryption.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         mode = sys.argv[2]
         dotnet_file = DotNetPE(PE)
         if "resources" in mode:
-                encrypted_data = get_PEstreams(dotnet_file) # Resouce stream are taking out of the .net bin containing the encrypted blob
+                encrypted_data = get_PEstreams(dotnet_file) # Resouce stream is taking out of the .net bin containing the encrypted blob
         key = b64decode("UZvlWL382SgvCZYlj18UTZUOUtJMFPCo9FZJzxSxiHw=")
         iv = b64decode("RqIrsHLwODhnyJldiyQd5g==")
         aes_decrypt(encrypted_data, key, iv) # Encrypted data is decrypted and then written to disk.
