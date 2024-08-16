f47f63f1afe2cd5e485a7374b85919d6d1a638be
Shawn <41995051+xephora@users.noreply.github.com>
2024-08-16 09:35:41 -0400

    Update OneLaunch-Remediation-Script.ps1

--- OneLaunch/OneLaunch-Remediation-Script.ps1
+++ OneLaunch/OneLaunch-Remediation-Script.ps1
@@ -40,16 +40,11 @@ foreach ($user in $user_list) {
             }
         }
     }
-    $localPaths = @(
-        "C:\Users\$user\appdata\local\OneLaunch",
-        "C:\Users\$user\appdata\Roaming\Microsoft\Windows\Start Menu\Programs\OneLaunch"
-    )
-    foreach ($localPath in $localpaths) {
+    $localPath = "C:\Users\$user\appdata\local\OneLaunch"
+    if (Test-Path -Path $localPath) {
+        Remove-Item $localPath -Force -Recurse -ErrorAction SilentlyContinue
         if (Test-Path -Path $localPath) {
-            Remove-Item $localPath -Force -Recurse -ErrorAction SilentlyContinue
-            if (Test-Path -Path $localPath) {
-                Write-Host "Failed to remove OneLaunch -> $localPath"
-            }
+            Write-Host "Failed to remove OneLaunch -> $localPath"
         }
     }
 }
@@ -57,6 +52,21 @@ foreach ($user in $user_list) {
 $sid_list = Get-Item -Path "Registry::HKU\S-*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+" | ForEach-Object { $_.ToString().Trim() }
 foreach ($sid in $sid_list) {
     if ($sid -notlike "*_Classes*") {
+        $registryPath = "Registry::$sid\SOFTWARE\Microsoft\Windows\CurrentVersion\UFH\SHC"
+        if (Test-Path $registryPath) {
+            $properties = Get-ItemProperty -Path $registryPath
+            foreach ($property in $properties.PSObject.Properties) {
+                if ($property.Value -is [array] -and ($property.Value -match $keyword)) {
+                    try {
+                        Remove-ItemProperty -Path $registryPath -Name $property.Name -ErrorAction Stop
+                    } catch {
+                        Write-Host "Failed to remove OneLaunch: $($property.Name) from $registryPath - $_"
+                    }
+                }
+            }
+        } else {
+            Write-Host "Registry path does not exist: $registryPath"
+        }
         $uninstallKey = "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Uninstall\{4947c51a-26a9-4ed0-9a7b-c21e5ae0e71a}_is1"
         if (Test-Path $uninstallKey) {
             Remove-Item $uninstallKey -Recurse -ErrorAction SilentlyContinue
@@ -66,10 +76,43 @@ foreach ($sid in $sid_list) {
         }
         $runKeys = @(
             "OneLaunch",
-            "OneLaunchChromium"
+            "OneLaunchChromium",
+            "OneLaunchUpdater"
         )
         foreach ($key in $runKeys) {
             $keypath = "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Run"
+            if ((Get-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue)) {
+                Remove-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue
+                if ((Get-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue)) {
+                    Write-Host "Failed to remove OneLaunch => $keypath.$key"
+                }
+            }
+        }
+
+        $miscKeys = @(
+            "OneLaunchHTML_.pdf",
+            "OneLaunch"
+        )
+        foreach ($key in $miscKeys) {
+            $keypath = "Registry::$sid\SOFTWARE\Microsoft\Windows\CurrentVersion\ApplicationAssociationToasts"
+            if ((Get-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue)) {
+                Remove-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue
+                if ((Get-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue)) {
+                    Write-Host "Failed to remove OneLaunch => $keypath.$key"
+                }
+            }
+        }
+        foreach ($key in $miscKeys) {
+            $keypath = "Registry::$sid\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppBadgeUpdated"
+            if ((Get-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue)) {
+                Remove-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue
+                if ((Get-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue)) {
+                    Write-Host "Failed to remove OneLaunch => $keypath.$key"
+                }
+            }
+        }
+        foreach ($key in $miscKeys) {
+            $keypath = "Registry::$sid\SOFTWARE\RegisteredApplications"
             if ((Get-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue)) {
                 Remove-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue
                 if ((Get-ItemProperty -Path $keypath -Name $key -ErrorAction SilentlyContinue)) {
@@ -113,6 +156,16 @@ $taskCacheKeys = @(
     "Registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\OneLaunchUpdateTask"
 )
 foreach ($taskCacheKey in $taskCacheKeys) {
+    if (Test-Path -Path $taskCacheKey) {
+        Remove-Item -Path $taskCacheKey -Recurse -ErrorAction SilentlyContinue
+    }
+}
+
+$traceCacheKeys = @(
+    "Registry::HKLM\SOFTWARE\Microsoft\Tracing\onelaunch_RASMANCS",
+    "Registry::HKLM\SOFTWARE\Microsoft\Tracing\onelaunch_RASAPI32"
+)
+foreach ($taskCacheKey in $traceCacheKeys) {
     if (Test-Path -Path $taskCacheKey) {
         Remove-Item -Path $taskCacheKey -Recurse -ErrorAction SilentlyContinue
     }
