f093a0ee591dc906f6bea16c7b61b6c2bf01bc1e
Shawn <41995051+xephora@users.noreply.github.com>
2024-09-24 22:33:07 -0400

    Update WaveBrowser-Remediation-Script-Win10.ps1

--- WaveBrowser/WaveBrowser-Remediation-Script-Win10.ps1
+++ WaveBrowser/WaveBrowser-Remediation-Script-Win10.ps1
@@ -11,17 +11,18 @@ if ($process) {
 Start-Sleep -Seconds 2
 
 $user_list = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
-foreach ($i in $user_list) {
-    if ($i -notlike "*Public*") {
+foreach ($username in $user_list) {
+    if ($username -notlike "*Public*") {
         $paths = @(
-            "C:\users\$i\Wavesor Software",
-            "C:\users\$i\WebNavigatorBrowser",
-            "C:\users\$i\appdata\local\WaveBrowser",
-            "C:\users\$i\appdata\local\WebNavigatorBrowser",
-            "C:\users\$i\downloads\Wave Browser*.exe",
-            "C:\users\$i\appdata\roaming\microsoft\windows\start menu\programs\WaveBrowser.lnk",
-            "C:\USERS\$i\APPDATA\ROAMING\MICROSOFT\INTERNET EXPLORER\QUICK LAUNCH\WAVEBROWSER.LNK",
-            "C:\USERS\$i\DESKTOP\WAVEBROWSER.LNK"
+            "C:\users\$username\Wavesor Software",
+            "C:\users\$username\WebNavigatorBrowser",
+            "C:\users\$username\appdata\local\WaveBrowser",
+            "C:\users\$username\appdata\local\WebNavigatorBrowser",
+            "C:\users\$username\downloads\Wave Browser*.exe",
+            "C:\users\$username\appdata\roaming\microsoft\windows\start menu\programs\WaveBrowser.lnk",
+            "C:\USERS\$username\APPDATA\ROAMING\MICROSOFT\INTERNET EXPLORER\QUICK LAUNCH\WAVEBROWSER.LNK",
+            "C:\USERS\$username\DESKTOP\WAVEBROWSER.LNK",
+            "C:\Users\$username\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\WaveBrowser.lnk"
         )
         foreach ($path in $paths) {
             if (Test-Path -Path $path) {
@@ -50,17 +51,20 @@ foreach ($taskPath in $taskPaths) {
 }
 
 $sid_list = Get-Item -Path "Registry::HKU\S-*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+" | ForEach-Object { $_.ToString().Trim() }
-foreach ($i in $sid_list) {
-    if ($i -notlike "*_Classes*") {
+foreach ($sid in $sid_list) {
+    if ($sid -notlike "*_Classes*") {
         $keys = @(
             "WaveBrowser",
             "Wavesor",
             "WebNavigatorBrowser",
             "Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser",
-            "Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser"
+            "Microsoft\Windows\CurrentVersion\App Paths\wavebrowser.exe",
+            "Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\WaveBrowser",
+            "Classes\CLSID\{9CD78CBC-FD21-4FFF-B452-9D792A58B7C4}\LocalServer32",
+            "Clients\StartMenuInternet\WaveBrowser.5QMLTPZDDJG2BQZHV26QUN4ZK4"
         )
         foreach ($key in $keys) {
-            $keypath = "Registry::$i\Software\$key"
+            $keypath = "Registry::$sid\Software\$key"
             if (Test-Path -Path $keypath) {
                 Remove-Item $keypath -Recurse -ErrorAction SilentlyContinue
                 if (Test-Path -Path $keypath) {
@@ -69,7 +73,15 @@ foreach ($i in $sid_list) {
             }
         }
         $runKey = "Wavesor SWUpdater"
-        $keypath = "Registry::$i\Software\Microsoft\Windows\CurrentVersion\Run"
+        $keypath = "Registry::$sid\Software\Microsoft\Windows\CurrentVersion\Run"
+        if ((Get-ItemProperty -Path $keypath -Name $runKey -ErrorAction SilentlyContinue)) {
+            Remove-ItemProperty -Path $keypath -Name $runKey -ErrorAction SilentlyContinue
+            if ((Get-ItemProperty -Path $keypath -Name $runKey -ErrorAction SilentlyContinue)) {
+                Write-Host "WaveBrowser Removal Unsuccessful => $keypath.$runKey"
+            }
+        }
+        $runKey = "WaveBrowser.5QMLTPZDDJG2BQZHV26QUN4ZK4"
+        $keypath = "Registry::$sid\Software\RegisteredApplications"
         if ((Get-ItemProperty -Path $keypath -Name $runKey -ErrorAction SilentlyContinue)) {
             Remove-ItemProperty -Path $keypath -Name $runKey -ErrorAction SilentlyContinue
             if ((Get-ItemProperty -Path $keypath -Name $runKey -ErrorAction SilentlyContinue)) {
@@ -80,8 +92,8 @@ foreach ($i in $sid_list) {
 }
 
 $sid_list = Get-Item -Path "Registry::HKU\*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+" | ForEach-Object { $_.ToString().Trim() }
-foreach ($i in $sid_list) {
-    if ($i -like "*_Classes*") {
+foreach ($sid in $sid_list) {
+    if ($sid -like "*_Classes*") {
         $classKeys = @(
             "WaveBrwsHTM*",
             "WavesorSWUpdater.CredentialDialogUser",
@@ -93,12 +105,16 @@ foreach ($i in $sid_list) {
             "WavesorSWUpdater.Update3COMClassUser",
             "WavesorSWUpdater.Update3COMClassUser.1.0",
             "WavesorSWUpdater.Update3WebUser",
-            "WavesorSWUpdater.Update3WebUser.1.0"
+            "WavesorSWUpdater.Update3WebUser.1.0",
+            "CLSID\{9CD78CBC-FD21-4FFF-B452-9D792A58B7C4}\LocalServer32"
         )
         foreach ($classKey in $classKeys) {
-            $classPath = "Registry::$i\$classKey"
+            $classPath = "Registry::$sid\$classKey"
             if (Test-Path -Path $classPath) {
                 Remove-Item $classPath -Recurse -ErrorAction SilentlyContinue
+                if (Test-Path -Path $classPath) {
+                    Write-Host "WaveBrowser Removal Unsuccessful => $classPath"
+                }
             }
         }
     }
