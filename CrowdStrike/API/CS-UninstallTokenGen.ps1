dc4b3d7dd09cb065e35ccab8b24dda47b3f410c8
Shawn <41995051+xephora@users.noreply.github.com>
2025-04-07 17:52:38 -0400

    Update CS-UninstallTokenGen.ps1

--- CrowdStrike/API/CS-UninstallTokenGen.ps1
+++ CrowdStrike/API/CS-UninstallTokenGen.ps1
@@ -14,13 +14,13 @@
     Path to a text file (one hostname per line) or CSV with a 'hostname' column.
 
 .EXAMPLE
-    PS> .\CrowdStrikeUninstall.ps1 -Hostname hostname
+    PS> CS-UninstallTokenGen.ps1 -Hostname hostname
 
 .EXAMPLE
-    PS> .\CrowdStrikeUninstall.ps1 -Hostfile .\hosts.csv
+    PS> CS-UninstallTokenGen.ps1 -Hostfile .\hosts.csv
 #>
 
-# After reviewing the source code for psfalcon sensor script, I was able to reconstruct a working poc designed for external tools such as Microsoft Intunes. 
+# After reviewing the source code for psfalcon sensor script, I was able to reconstruct a working poc designed for external tools such as Microsoft Intunes. It passively retrieves the uninstall maintenance token from a given hostname.
 # https://github.com/CrowdStrike/psfalcon/blob/f0b3a683b0e1d17857b1785b5e7da06097d7714d/public/psf-sensors.ps1#L323
 # Requirements: Refer to PSFalcon's wiki https://github.com/CrowdStrike/psfalcon/wiki/Uninstall-FalconSensor
 param(
